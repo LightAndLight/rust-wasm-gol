8f5d884ef5d95efb1842d83dd5333dab6714f3d0
Isaac Elliott <isaace71295@gmail.com>
2021-05-10 17:36:54 +1000

    shell.nix: provide environment for build_and_serve

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
 { pkgs ? import ./nix/pkgs {} }:
 pkgs.mkShell {
+  NODE_PATH="${pkgs.nodePackages.webpack}/lib/node_modules:${pkgs.nodePackages.copy-webpack-plugin}/lib/node_modules";
   buildInputs = with pkgs; [
     wasm-pack
     wabt
+    nodePackages.npm
+    nodePackages.webpack-cli
+    python3
   ];
 }
